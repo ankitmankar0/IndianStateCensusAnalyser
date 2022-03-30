d1a75de6c0bb918385dc000c861c1e37920f4eb2
ankitmankar0 <ankitmankar012@gmail.com>
2022-03-30 10:01:11 +0530

    india state sensus

---
+++ IndianStateCensusAnalyser/DTO/CensusDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser.POCO
+{
+    public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int tin;
+        public string stateCode;
+        public long population;
+        public long area;
+        public long density;
+        public long housingUnits;
+        public double totalArea;
+        public double waterArea;
+        public double landArea;
+        public double populationDensity;
+        public double housingDensity;
+
+        public CensusDTO(StateCodeDAO stateCodeDAO)
+        {
+            this.serialNumber = stateCodeDAO.serialNumber;
+            this.stateName = stateCodeDAO.stateName;
+            this.tin = stateCodeDAO.tin;
+            this.stateCode = stateCodeDAO.stateCode;
+        }
+        public CensusDTO(CensusDataDAO censusDataDao)
+        {
+            this.state = censusDataDao.state;
+            this.population = censusDataDao.population;
+            this.area = censusDataDao.area;
+            this.density = censusDataDao.density;
+        }
+        //public CensusDTO(USCensusDAO usCensusDAO)
+        //{ 
+        //    this.stateCode = usCensusDAO.stateId;
+        //    this.stateName = usCensusDAO.stateName;
+        //    this.population = usCensusDAO.population;
+        //    this.housingUnits = usCensusDAO.housingUnits;
+        //    this.totalArea = usCensusDAO.totalArea;
+        //    this.waterArea = usCensusDAO.waterArea;
+        //    this.landArea = usCensusDAO.landArea;
+        //    this.populationDensity = usCensusDAO.populationDensity;
+        //    this.housingDensity = usCensusDAO.housingDensity;
+        //}
+    }
+}
